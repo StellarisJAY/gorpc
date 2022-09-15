e02d1c62b1702c510405bbe6e70a852b7201682a
StellarisJAY <760409558@qq.com>
2022-09-15 15:20:26 +0800

    test: 序列化单元测试以及benchmark

--- codec/codec_test.go
+++ codec/codec_test.go
@@ -52,18 +52,78 @@ func TestJSONCodec_Encode(t *testing.T) {
 	}
 }
 
-func TestProtobufCodec_Decode(t *testing.T) {
-
-}
-
 func TestProtobufCodec_Encode(t *testing.T) {
-
+	req := &PBRegisterRequest{
+		Age:      0,
+		Ratio:    1.34,
+		Account:  "jay",
+		Password: "123456",
+	}
+	data, err := ProtobufCodec{}.Encode(req)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	t.Log(data)
 }
 
 func BenchmarkJSONCodec_Decode(b *testing.B) {
-
+	codec := JSONCodec{}
+	data := []byte("{\"account\":\"jay\",\"password\":\"123456\",\"age\":0,\"ratio\":1.34}")
+	req := new(RegisterRequest)
+	for i := 0; i < b.N; i++ {
+		err := codec.Decode(data, req)
+		if err != nil {
+			b.Error(err)
+			b.FailNow()
+		}
+	}
 }
 
 func BenchmarkJSONCodec_Encode(b *testing.B) {
+	req := &RegisterRequest{
+		Account:  "jay",
+		Password: "123456",
+	}
+	age, ratio := 0, 1.34
+	req.Age = &age
+	req.Ratio = &ratio
+	codec := JSONCodec{}
+	for i := 0; i < b.N; i++ {
+		_, err := codec.Encode(req)
+		if err != nil {
+			b.Error(err)
+			b.FailNow()
+		}
+	}
+}
 
+func BenchmarkProtobufCodec_Decode(b *testing.B) {
+	data := []byte{10, 3, 106, 97, 121, 18, 6, 49, 50, 51, 52, 53, 54, 37, 31, 133, 171, 63}
+	codec := ProtobufCodec{}
+	req := new(PBRegisterRequest)
+	for i := 0; i < b.N; i++ {
+		err := codec.Decode(data, req)
+		if err != nil {
+			b.Error(err)
+			b.FailNow()
+		}
+	}
+}
+
+func BenchmarkProtobufCodec_Encode(b *testing.B) {
+	req := &PBRegisterRequest{
+		Age:      0,
+		Ratio:    1.34,
+		Account:  "jay",
+		Password: "123456",
+	}
+	codec := ProtobufCodec{}
+	for i := 0; i < b.N; i++ {
+		_, err := codec.Encode(req)
+		if err != nil {
+			b.Error(err)
+			b.FailNow()
+		}
+	}
 }
